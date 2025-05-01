a367657fb600c42b4de850a5fea1934af312b15e
muntazirshah <amuntazirshah124@gmail.com>
2025-05-01 17:08:52 +0500

    Add new changes

--- Firebase Contact App/src/config/firebase.js
+++ Firebase Contact App/src/config/firebase.js
@@ -1,15 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore}   from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Get the Firebase API Key from the environment variable
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyBI-qWNyuN7rSwMRHERb305dKlUvvpAYQ0",
   authDomain: "vite-contact-44e0e.firebaseapp.com",
   projectId: "vite-contact-44e0e",
-  storageBucket: "vite-contact-44e0e.appspot.com",
+  storageBucket: "vite-contact-44e0e.firebasestorage.app",
   messagingSenderId: "672216610691",
   appId: "1:672216610691:web:1d13afa175e1ab6df368b6",
+  measurementId: "G-YHCKKLZ7MB"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Initialize Firebase
+ export    const app = initializeApp(firebaseConfig);
+ export const db = getFirestore(app);
+const analytics = getAnalytics(app);
